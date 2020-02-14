cca76d84dee0702630a7eca0184906cb34642d36
Ziad Alzarka <ziadalzarka@gmail.com>
2020-02-14 02:21:25 +0200

    feat: calculate image

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const date = new Date();
 
-const day = date.getDate();
+date.setMonth(0);
+date.setDate(0);
+date.setHours(0, 0, 0, 0);
+
+let day = moment().diff(date, "days") % 27;
+
+if (day == 0) {
+  day = 27;
+}
+
+document.getElementById("drawing").src = `images/${day}.jpg`;
